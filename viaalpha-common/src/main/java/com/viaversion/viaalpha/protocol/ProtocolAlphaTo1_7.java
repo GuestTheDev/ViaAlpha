e8ce13f8b8ec7316af91aa4439ac8e74ea6cc328
GuestTheDev <kuklisolga461@gmail.com>
2025-10-26 18:59:57 +0400

    Update ProtocolAlphaTo1_7.java

--- viaalpha-common/src/main/java/com/viaversion/viaalpha/protocol/ProtocolAlphaTo1_7.java
+++ viaalpha-common/src/main/java/com/viaversion/viaalpha/protocol/ProtocolAlphaTo1_7.java
@@ -3,37 +3,33 @@ package com.viaversion.viaalpha.protocol;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.protocol.AbstractProtocol;
 import com.viaversion.viaversion.api.protocol.remapper.PacketHandlers;
-import com.viaversion.viaversion.api.protocol.packet.PacketWrapper;
-import com.viaversion.viaversion.api.type.Types;
 import com.viaversion.viaalpha.packet.ClientboundPacketsAlpha;
 import com.viaversion.viaalpha.packet.ServerboundPacketsAlpha;
 
-public class ProtocolAlphaTo1_7 extends AbstractProtocol<ClientboundPacketsAlpha, 
-                                                       com.viaversion.viaversion.protocols.v1_7_6to1_8.packet.ClientboundPackets1_8,
-                                                       ServerboundPacketsAlpha, 
-                                                       com.viaversion.viaversion.protocols.v1_7_6to1_8.packet.ServerboundPackets1_8> {
+// Временная упрощенная версия - будем использовать Object как целевые пакеты
+public class ProtocolAlphaTo1_7 extends AbstractProtocol<ClientboundPacketsAlpha, Object, ServerboundPacketsAlpha, Object> {
 
     public ProtocolAlphaTo1_7() {
-        super(ClientboundPacketsAlpha.class, 
-              com.viaversion.viaversion.protocols.v1_7_6to1_8.packet.ClientboundPackets1_8.class,
-              ServerboundPacketsAlpha.class, 
-              com.viaversion.viaversion.protocols.v1_7_6to1_8.packet.ServerboundPackets1_8.class);
+        super(ClientboundPacketsAlpha.class, Object.class, ServerboundPacketsAlpha.class, Object.class);
     }
 
     @Override
     protected void registerPackets() {
-        // Пока просто зарегистрируем пустые обработчики для компиляции
-        // Реальную логику добавим позже
+        // Базовые обработчики для компиляции
         registerClientbound(ClientboundPacketsAlpha.KEEP_ALIVE, new PacketHandlers() {
             @Override
             public void register() {
                 // TODO: KeepAlive транслятор
+                handler(wrapper -> {
+                    // Пока просто пропускаем пакет
+                    System.out.println("[ViaAlpha] KeepAlive packet received");
+                });
             }
         });
     }
 
     @Override
     public void init(UserConnection userConnection) {
-        // Инициализация соединения
+        // Базовая инициализация
     }
 }
