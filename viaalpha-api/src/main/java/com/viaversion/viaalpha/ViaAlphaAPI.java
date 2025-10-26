12a3b77a70b2c7f763c9f19e43ecf7b14053f0d4
GuestTheDev <kuklisolga461@gmail.com>
2025-10-26 18:18:05 +0400

    Create ViaAlphaAPI.java

---
+++ viaalpha-api/src/main/java/com/viaversion/viaalpha/ViaAlphaAPI.java
@@ -0,0 +1,17 @@
+package com.viaversion.viaalpha;
+
+public class ViaAlphaAPI {
+    private static ViaAlphaAPI instance;
+    
+    public static ViaAlphaAPI getInstance() {
+        return instance;
+    }
+    
+    public static void setInstance(ViaAlphaAPI instance) {
+        ViaAlphaAPI.instance = instance;
+    }
+    
+    public boolean isEnabled() {
+        return true;
+    }
+}
