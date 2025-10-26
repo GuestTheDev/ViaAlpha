74ed5291dc892958a3ad012753d490234bd7b348
GuestTheDev <kuklisolga461@gmail.com>
2025-10-26 19:13:54 +0400

    Update ClientboundPacketsAlpha.java

--- viaalpha-common/src/main/java/com/viaversion/viaalpha/packet/ClientboundPacketsAlpha.java
+++ viaalpha-common/src/main/java/com/viaversion/viaalpha/packet/ClientboundPacketsAlpha.java
@@ -3,13 +3,13 @@ package com.viaversion.viaalpha.packet;
 import com.viaversion.viaversion.api.protocol.packet.ClientboundPacketType;
 
 public enum ClientboundPacketsAlpha implements ClientboundPacketType {
-    KEEP_ALIVE(0x00),           // Пустой пакет
-    LOGIN(0x01),                // Логин
-    CHAT_MESSAGE(0x03),         // Сообщение в чат
-    SPAWN_POSITION(0x06),       // Позиция спавна
-    PLAYER_POSITION_AND_LOOK(0x0D), // Позиция и взгляд игрока
-    BLOCK_CHANGE(0x35),         // Изменение блока
-    DISCONNECT(0xFF);           // Отключение
+    KEEP_ALIVE(0x00),
+    LOGIN(0x01),
+    CHAT_MESSAGE(0x03),
+    SPAWN_POSITION(0x06),
+    PLAYER_POSITION_AND_LOOK(0x0D),
+    BLOCK_CHANGE(0x35),
+    DISCONNECT(0xFF);
 
     private final int id;
 
