a8e6d2255ee7574bb4ec1bfda5c90da857e1fab5
GuestTheDev <kuklisolga461@gmail.com>
2025-10-27 13:45:08 +0400

    Create ProtocolVersionAlpha.java

---
+++ viaalpha-api/src/main/java/com/viaversion/viaalpha/protocol/version/ProtocolVersionAlpha.java
@@ -0,0 +1,8 @@
+package com.viaversion.viaalpha.protocol.version;
+
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+
+public class ProtocolVersionAlpha {
+    // Версия протокола Alpha 1.2.6 (условный номер)
+    public static final ProtocolVersion ALPHA_1_2_6 = new ProtocolVersion(2, "Alpha 1.2.6");
+}
