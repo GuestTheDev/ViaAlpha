7ab85bac2e94b52219704ecef7b8e10ebd7f62d7
GuestTheDev <kuklisolga461@gmail.com>
2025-10-27 13:55:26 +0400

    Update VersionProviderAlpha.java

--- viaalpha-common/src/main/java/com/viaversion/viaalpha/protocol/version/VersionProviderAlpha.java
+++ viaalpha-common/src/main/java/com/viaversion/viaalpha/protocol/version/VersionProviderAlpha.java
@@ -7,12 +7,16 @@ import com.viaversion.viaversion.protocols.v1_8to1_9.ProtocolVersion1_8;
 public class VersionProviderAlpha implements VersionProvider {
     @Override
     public ProtocolVersion getClosestProtocolVersion(int clientVersion) {
+        System.out.println("[ViaAlpha] VersionProvider called for client version: " + clientVersion);
+        
         // Если клиент использует протокол Alpha (версия 2)
         if (clientVersion == 2) {
+            System.out.println("[ViaAlpha] Detected Alpha 1.2.6 client!");
             return ProtocolVersionAlpha.ALPHA_1_2_6;
         }
         
         // Для других версий используем стандартную логику
+        System.out.println("[ViaAlpha] Using default version detection");
         return ProtocolVersion1_8.getClosestProtocolVersion(clientVersion);
     }
 }
